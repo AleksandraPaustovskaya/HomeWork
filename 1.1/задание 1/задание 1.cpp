7ee16295752bfbef592020632d72b50e634aa745
Aleksandra_Paustovskaya <paustovskayasasha@gmail.com>
2019-12-09 19:43:24 +0300

    готовое

---
+++ 1.1/задание 1/задание 1.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	printf("vvedite znachenie x = ");
+
+	int x = 0;
+	scanf("%d", &x);
+
+	const int squareOfX = x * x;
+
+	const int result = ((squareOfX + 1) * (squareOfX + x) + 1) / 100;
+	printf("%d", result);
+
+	return 0;
+}
