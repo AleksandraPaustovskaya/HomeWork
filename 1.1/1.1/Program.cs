b1aa0c5588110e65185b6d0572fe650156283f66
Aleksandra_Paustovskaya <paustovskayasasha@gmail.com>
2020-02-25 16:09:21 +0300

    Первая версия

---
+++ 1.1/1.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string enter = Console.ReadLine();
+            int factorial = Convert.ToInt32(enter);
+            if (factorial > 12)
+            {
+                Console.WriteLine("Over the limit number");
+            }
+            else
+            {
+                int helper = factorial;
+                while (helper > 1)
+                {
+                    helper--;
+                    factorial = factorial * helper;
+                }
+                Console.WriteLine(factorial);
+            }
+        }
+    }
+}
