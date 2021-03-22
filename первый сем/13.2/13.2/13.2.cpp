84380bd39c4ee355bef654ba37b5ee644c47d934
Aleksandra_Paustovskaya <paustovskayasasha@gmail.com>
2021-03-22 16:29:51 +0300

    13.2 первая версия

---
+++ первый сем/13.2/13.2/13.2.cpp
@@ -0,0 +1,27 @@
+﻿// С помощью ДКА с явной таблицей состояний, заданной в файле,
+//  вывести на консоль все комментарии С++ вида /* комментарий */ из входного файла (вместе с символами "/* */").
+//
+
+#include <stdio.h>
+#include <string.h>
+#include "searchComments.h"
+
+bool test()
+{
+	char fileName[] = "test.txt";
+	char answer[] = "/*ads/*dsf***/\n/*qwe/*/\n";
+	return strcmp(comments(fileName), answer) == 0;
+}
+
+int main()
+{
+	if (!test())
+	{
+		printf("Testing error\n");
+		return 1;
+	}
+
+	char fileName[] = "input.txt";
+	printf("Comments from file:\n%s", comments(fileName));
+	return 0;
+}
